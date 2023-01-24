7878aec158557925ba71020de551a855f3ff0ecc
Vitalii <lennab@mail.ru>
2023-01-24 21:38:31 +0300

    Выполнены задания с 1 по 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void Task1_3() {
+/**/
+        System.out.println("Task1:");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
 
+        System.out.println("\nTask2:");
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        System.out.println("\nTask3:");
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника — " + fullName.replace('ё', 'е'));
+    }
+
+    public static void main(String[] args) {
+        Task1_3();
     }
 }
